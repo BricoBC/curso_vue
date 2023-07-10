6441b69c7c6d1164fb1ab3db64fc8aa395cdd56d
BricoBC <barajas.c.bruno.n@gmail.com>
2023-07-10 10:48:02 -0600

    Componentes

--- src/main.js
+++ src/main.js
@@ -5,8 +5,9 @@ import App from './App.vue';
 import MyGym from './MyGym.vue';
 import Computed from './Computed.vue';
 import Ciclo from './Cycle.vue';
-
+import PageOne from './PageOne.vue'
 // createApp(App).mount('#app')
 // createApp(MyGym).mount('#myGym')
 // createApp(Computed).mount('#computed')
 createApp(Ciclo).mount('#ciclo')
+createApp(PageOne).mount('#pagina')
