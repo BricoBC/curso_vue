de0a09c8c91dff3f005f6c9494d5e697849568c1
BricoBC <barajas.c.bruno.n@gmail.com>
2023-07-10 10:05:30 -0600

    Lifecycle

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
-import MyGym from './MyGym.vue'
-import Computed from './Computed.vue'
+import App from './App.vue';
+import MyGym from './MyGym.vue';
+import Computed from './Computed.vue';
+import Ciclo from './Cycle.vue';
 
 // createApp(App).mount('#app')
 // createApp(MyGym).mount('#myGym')
-createApp(Computed).mount('#computed')
+// createApp(Computed).mount('#computed')
+createApp(Ciclo).mount('#ciclo')
